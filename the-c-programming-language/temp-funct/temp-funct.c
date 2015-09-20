f39269ef97e67e5bccb60fe99b94f8eefab1c315
Ethan R. Jones <doubleplusgood23@gmail.com>
2015-09-20 03:47:53 -0400

    Modified temp program - now uses a function.

---
+++ the-c-programming-language/temp-funct/temp-funct.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define LOWER 0  /* Lower limit of table */
+#define UPPER 300 /* upper limit */
+#define STEP  20 /* step size */
+
+void calc();
+
+/* print Fahrenheit-Celsius table */
+int main()
+{
+    calc();
+    return 0;
+}
+
+/* do the fahr to cel calculating */
+void calc()
+{
+    int fahr;
+    for(fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+	printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+}
