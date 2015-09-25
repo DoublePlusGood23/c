ee96cc2e539788b86d257237426a5082271ae4eb
Ethan R. Jones <doubleplusgood23@gmail.com>
2015-09-25 15:49:20 -0400

    data-size program

---
+++ the-c-programming-language/ch2/data-size/data-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+// Find the size of data types
+
+int main()
+{
+    printf("float: %f \n", sizeof(float));
+    printf("double: %d \n", sizeof(double));
+}
